85c1030cb2c2dfdf5a509c5d35b04df1f8ef87b0
Grasiela Texeira <grasy.mst@gmail.com>
2022-07-26 14:29:53 -0300

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@
 ?>
   <div id="main-container" class="container-fluid">
     <h2 class="section-title">Dashboard</h2>
-    <p class="section-description">Adicione ou atualize as informações do filmes que você enviou</p>
+    <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou</p>
     <div class="col-md-12" id="add-movie-container">
       <a href="<?= $BASE_URL ?>newmovie.php" class="btn card-btn">
         <i class="fas fa-plus"></i> Adicionar Filme
